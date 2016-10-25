4003726baa3b3b33d004027fca0287f5ec0af7da
Snehal Jadhav <snehalja@cybage.com>
2016-10-25 12:04:20 +0530

    webservice

---
+++ src/main/java1/com/WebHibernate/WebService/dao/DataDao.java
@@ -0,0 +1,18 @@
+package com.WebHibernate.WebService.dao;
+
+import java.util.List;
+
+import com.WebHibernate.WebService.model.Employee;
+
+public interface DataDao {
+	
+	public boolean addEntity(Employee employee) throws Exception;
+	
+	public Employee getEnityById(long id) throws Exception;
+	
+	public List getEntityList() throws Exception;
+	
+	public boolean deleteEntity(long id) throws Exception;
+	
+
+}
